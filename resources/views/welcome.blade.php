787bd1921fb6b841fb7d43917dbde8ca02e51756
Marcos Petzinger de Oliveira <75101581+marcosptz@users.noreply.github.com>
2021-07-25 02:19:53 -0300

    ajuste na view welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -51,36 +51,6 @@
                 </div>
               </div>
             </div>
-
-            {{-- <div class="conteudo">
-                <div class="card-columns d-flex justify-content-center">
-                    <div class="card text-center shadow p-3 mb-5 bg-white rounded" >
-                        <img class="card-img-top" src="{{ URL::to('/imagens/cadastro.png') }}" alt="">
-                        <div class="card-body">
-                            <h4 class="card-title">Novo Atendimento</h4>
-                            <p class="card-text"></p>
-                            <a href="" class="btn btn-outline-secondary  stretched-link">Cadastro</a>
-                        </div>
-                    </div>
-                    <div class="card text-center shadow p-3 mb-5 bg-white rounded" >
-                        <img class="card-img-top" src="{{ URL::to('/imagens/pesquisa.png') }}" alt="">
-                        <div class="card-body">
-                            <h4 class="card-title">Lista de Atendimentos</h4>
-                            <p class="card-text"></p>
-                            <a href="" class="btn btn-outline-secondary stretched-link">Lista</a>
-                        </div>
-                    </div>
-                    <div class="card text-center shadow p-3 mb-5 bg-white rounded" >
-                        <img class="card-img-top" src="{{ URL::to('/imagens/pesquisa.png') }}" alt="">
-                        <div class="card-body">
-                            <h4 class="card-title">Acompanhamentos</h4>
-                            <p class="card-text"></p>
-                            <a href=""
-                                class="btn btn-outline-secondary stretched-link">Lista</a>
-                        </div>
-                    </div>
-                </div>
-            </div> --}}
         </div>
       </div>
     </div>
